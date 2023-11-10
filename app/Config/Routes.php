2253d9705d2508e6891b545493ca52fb536809a8
SubektiBimoWicaksono <bimoputraw27@gmail.com>
2023-11-10 15:27:02 +0700

    Pertemuan 8

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -5,6 +5,7 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
+
 $routes->get('/', 'Home::index');
 $routes->get('insertproducts', 'ProductController::insertPage');
 $routes->post('insertproduct', 'ProductController::insertProduct');
@@ -16,4 +17,7 @@ $routes->get('delete-product/(:any)', 'ProductController::deleteProduct/$1');
 $routes->group('api', function ($routes){
     $routes->get('products', 'ProductController::readProductApi');
     $routes->get('product/(:any)', 'ProductController::getProductApi/$1');
+    $routes->post('insert-product', 'ProductController::insertProductApi');
+    $routes->put('update-product-api/(:num)', 'ProductController::updateProductApi/$1');
+    $routes->delete('delete-product-api/(:num)', 'ProductController::deleteProductApi/$1');
 });
--- app/Controllers/ProductController.php
+++ app/Controllers/ProductController.php
@@ -20,6 +20,45 @@ class ProductController extends BaseController {
         return redirect()->to('products');
     }
 
+    public function insertProductApi(){
+        $requestData = $this->request->getJSON();
+        $validation = $this->validate([
+            'nama_product' => 'required',
+            'description' => 'required',
+        ]);
+    
+        if(!$validation) {
+            $this->response->setStatusCode(400);
+            return $this->response->setJSON([
+                'code'=> 400,
+                'status'=>'BAD REQUEST',
+                'data'=>'produk belum ditambahkan'
+            ]);
+        }
+    
+        $data = [
+            'nama_product' => $requestData->nama_product, // Correct property name
+            'description' => $requestData->description,
+        ];
+    
+        $insert = $this->product->insertProductORM($data);
+        if ($insert) {
+            return $this->respond([
+                'code' => 200,
+                'status' => 'OK',
+                'data' => $data
+            ]);
+        } else {
+        $this->response->setStatusCode(500);
+        return $this->response->setJSON([
+            'code'=> 500,
+            'status'=>'INTERNAL SERVER ERROR',
+            'data'=>'null'
+            ]);
+        }
+    }
+    
+
     public function insertPage(){
         return view('insert_product');
     }
@@ -79,9 +118,103 @@ class ProductController extends BaseController {
         return redirect()->to(base_url("products"));
     }
 
+    public function updateProductApi($id){
+        $requestData = $this->request->getJSON();
+        $validation = $this->validate([
+            'nama_product' => 'required',
+            'description' => 'required',
+        ]);
+    
+        if (!$validation) {
+            $this->response->setStatusCode(400);
+            return $this->response->setJSON([
+                'code' => 400,
+                'status' => 'BAD REQUEST',
+                'data' => 'Invalid input data'
+            ]);
+        }
+    
+        $data = [
+            'nama_product' => $requestData->nama_product,
+            'description' => $requestData->description,
+        ];
+    
+        // Check if the product exists
+        $existingProduct = $this->product->find($id);
+    
+        if (!$existingProduct) {
+            $this->response->setStatusCode(404);
+            return $this->response->setJSON([
+                'code' => 404,
+                'status' => 'NOT FOUND',
+                'data' => 'Product not found'
+            ]);
+        }
+
+            $update = $this->product->update($id, $data);
+    
+            if ($update) {
+                return $this->respond([
+                    'code' => 200,
+                    'status' => 'OK',
+                    'data' => $data
+                ]);
+            } else {
+                $this->response->setStatusCode(500);
+                return $this->response->setJSON([
+                    'code'=> 500,
+                    'status'=>'INTERNAL SERVER ERROR',
+                    'data'=>'null'
+                    ]);
+                }
+        
+            $this->response->setStatusCode(500);
+            return $this->response->setJSON([
+                'code' => 500,
+                'status' => 'INTERNAL SERVER ERROR',
+                'data' => $e->getMessage()
+            ]);
+        
+    }
+    
+    
+
     public function deleteProduct($id){
         $this->product->delete($id);
         return redirect()->to(base_url("products"));
 
     }
+
+    public function deleteProductApi($id) {
+        // Check if the product exists
+        $existingProduct = $this->product->find($id);
+    
+        if (!$existingProduct) {
+            $this->response->setStatusCode(404);
+            return $this->response->setJSON([
+                'code' => 404,
+                'status' => 'NOT FOUND',
+                'data' => 'Product not found'
+            ]);
+        }
+    
+        // Delete the product and handle errors
+        try {
+            $this->product->delete($id);
+    
+            return $this->respond([
+                'code' => 200,
+                'status' => 'OK',
+                'data' => 'Product deleted successfully'
+            ]);
+        } catch (\Exception $e) {
+            $this->response->setStatusCode(500);
+            return $this->response->setJSON([
+                'code' => 500,
+                'status' => 'INTERNAL SERVER ERROR',
+                'data' => $e->getMessage()
+            ]);
+        }
+    }
+    
 }
